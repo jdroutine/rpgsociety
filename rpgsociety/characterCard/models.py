2b53a6a0b486a092aff1d7d664ac830844e195b9
jdroutine <jd.routine@gmail.com>
2021-07-04 10:26:05 +0100

    Character model upgraded

--- rpgsociety/characterCard/models.py
+++ rpgsociety/characterCard/models.py
@@ -4,11 +4,22 @@ from django.db import models
 
 
 class Character(models.Model):
+    # status choices
+    statusChoices = (
+        ('Normalny', 'Normal'),
+        ('Zdenerwowanie', 'Nervousness'),
+        ('Przerażenie', 'Horror'),
+        ('Zmęczenie', 'Fatigue'),
+        ('Zranienie', 'Injury'),
+        ('Wycieńczenie', 'Weakness')
+    )
+
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=100)
     age = models.CharField(max_length=3)
+    status = models.CharField(max_length=50, choices=statusChoices, null=True)
+    luck = models.IntegerField(null=True)
+    item = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.name
-
-
