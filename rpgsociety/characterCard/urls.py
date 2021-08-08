93617d3288d36b66b88a0addbc26247f126a6b51
jdroutine <jd.routine@gmail.com>
2021-08-08 22:41:24 +0100

    added gallery

--- rpgsociety/characterCard/urls.py
+++ rpgsociety/characterCard/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-#from . import views
-from .views import CharacterDetailView, CharacterView, HomeView, MapView
+from . import views
+from .views import CharacterDetailView, CharacterView, HomeView, MapView, gallery
 
 
 urlpatterns = [
-    #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
     path('character', CharacterView.as_view(), name="character"),
     path('character/<int:pk>', CharacterDetailView.as_view(), name="characterDetail"),
     path('map', MapView.as_view(), name="map"),
+    path('gallery', views.gallery, name="gallery"),
 ]
